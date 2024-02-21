d5e244a5a7e35dc884ba977a2aff3143bb870732
“corneliusthefirst” <ndeffo.jugal98@gmail.com>
2024-02-21 01:12:30 +0100

    added scroll to bottom of accordion boby on open accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 import UpIcon from "./Icons/up-icon"
 interface AccordionProps {
   title: string
@@ -7,8 +7,10 @@ interface AccordionProps {
 
 const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const accordionRef = useRef<HTMLDivElement>(null)
   const toggleAccordion = () => {
     setIsOpen(!isOpen)
+    accordionRef.current?.scrollIntoView({ behavior: "smooth", block: "end" })
   }
 
   return (
@@ -17,7 +19,9 @@ const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
         className="flex justify-between items-center px-4 py-2 bg-transparent text-gray-800 w-full"
         onClick={toggleAccordion}
       >
-        <span className="font-semibold  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text">{title}</span>
+        <span className="font-semibold  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text">
+          {title}
+        </span>
         <span
           className={`transform transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`}
         >
@@ -25,9 +29,11 @@ const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
         </span>
       </button>
       <div
-        className={`overflow-scroll transition-max-height duration-300 ${isOpen ? "max-h-[320px] md:max-h-[520px]" : "max-h-0"}`}
+        className={`overflow-scroll scroll-smooth scroll-pl-1 snap-y transition-max-height duration-300 ${isOpen ? "max-h-[320px] md:max-h-[480px]" : "max-h-0"}`}
       >
-        <div className="px-4 py-2">{children}</div>
+        <div ref={accordionRef} className="px-4 py-2">
+          {children}
+        </div>
       </div>
     </div>
   )
